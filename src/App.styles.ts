012c6f7e7b9e1680ca1c09af7dbd0cb782775cf0
vitormsiqueira <vmsvitor@outlook.com>
2022-04-23 20:18:56 -0400

    Add map, character and moves

---
+++ src/App.styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: #24282F;
+    min-height: 100vh;
+    color: #fff;
+`;
+
+
+export const Map = styled.div`
+    width: 480px;
+    height: 480px;
+    background-image: url('./assets/map.png');
+    backgorund-position: center;
+    background-size: 100%;
+`;
